12f7bc9253f5269993953b9191881dc1c4317195
Ashar Shahid <Ashar2shahid@gmail.com>
2021-04-16 21:12:51 -0400

    updated example

--- nice_example.c
+++ nice_example.c
@@ -3,31 +3,35 @@
 #include "user.h"
 #include "fcntl.h"
 
-int
-main(int argc, char *argv[])
-{
-    int  k, id;
-    double i,x;
-    x=0;
-    id = 0;
-    for (k = 0; k < 20;k++){
-        id = fork();
-        if(id < 0){
-            printf(1,"%d failed to fork \n", getpid() );
-        }
-        else if (id > 0) {
-            //In Parent Process 
-            printf(1,"Parent %d creating child %d \n", getpid(), id);
-            wait();
-        }
-        else {
-            //In Child Process
-            printf(1, "Child %d created \n", getpid());
-            for (i=0;i<99999999;i++){
-                x = x + 10.11 * 11.11; // Calculation to consume CPU time.
-            }
-            break;
-        }
-    }
-    exit();
+int main(int argc, char *argv[]) {
+  int pid;
+  int k, n;
+  int x, z;
+
+  if(argc < 2)
+	n = 1; //Default
+  else
+	n = atoi(argv[1]);
+  if (n < 0 ||n > 20)
+	n = 2;
+  x = 0;
+  pid = 0;
+
+  for ( k = 0; k < n; k++ ) {
+    pid = fork ();
+    if ( pid < 0 ) {
+      printf(1, "%d failed in fork!\n", getpid());
+    } else if (pid > 0) {
+      // parent
+      printf(1, "Parent %d creating child %d\n",getpid(), pid);
+      wait();
+      }
+      else{
+	printf(1,"Child %d created\n",getpid());
+	for(z = 0; z < 4000000000; z+=1)
+	    x = x + 3.14*89.64; //Useless calculation to consume CPU Time
+	break;
+      }
+  }
+  exit();
 }
