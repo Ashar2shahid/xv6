b32c3a6db6fdc1433ac5a4842b4e82df6276ddc3
Ashar Shahid <Ashar2shahid@gmail.com>
2021-04-21 21:15:07 -0400

    updated test

--- lotterytest3.c
+++ lotterytest3.c
@@ -21,7 +21,7 @@ main(int argc, char *argv[])
         nice(1); // set priority to 5
         k++;
         z += 3.14 * 89.9; // occupy cpu time
-        if(k%1000 == 0)
+        if(k%1000000 == 0)
         printf(1, "Process 1 -Priority=%d -tickets= %d -ticks=%d \n",nice(0),20-nice(0)+1,k);
     }
   }
@@ -31,11 +31,11 @@ main(int argc, char *argv[])
     while(1) {
       if (nice(0) <= 4)
         nice(1); // set priority to 5
-        if (k == 50000)
+        if (k == 10000000)
             nice(8); // Half the number of tickets
         z += 3.14 * 89.9; // occupy cpu time
         k++;
-        if(k%1000 == 0)
+        if(k%1000000 == 0)
         printf(1, "Process 2 -Priority=%d -tickets= %d -ticks=%d \n",nice(0),20-nice(0)+1,k);
     }
   }
